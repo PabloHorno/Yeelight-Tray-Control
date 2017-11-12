f0d0176066e041b212cd3752301063aa5f6f3fa8
Pablo Horno <pablojhorno@gmail.com>
2017-11-12 01:37:35 +0100

    Add project files.

---
+++ Yeelight/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Yeelight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Luz = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porcentajeToolStripMenuItem = new Yeelight.Form1.TrackBarMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Luz
+            // 
+            this.Luz.Icon = ((System.Drawing.Icon)(resources.GetObject("Luz.Icon")));
+            this.Luz.Text = "Luz";
+            this.Luz.Visible = true;
+            this.Luz.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.Color.White;
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.porcentajeToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(213, 96);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // porcentajeToolStripMenuItem
+            // 
+            this.porcentajeToolStripMenuItem.Name = "porcentajeToolStripMenuItem";
+            this.porcentajeToolStripMenuItem.Size = new System.Drawing.Size(152, 45);
+            this.porcentajeToolStripMenuItem.Text = "Porcentaje";
+            this.porcentajeToolStripMenuItem.Click += new System.EventHandler(this.porcentajeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon Luz;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private TrackBarMenuItem porcentajeToolStripMenuItem;
+    }
+}
+
---
+++ Yeelight/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms.Design;
+
+namespace Yeelight
+{
+    public partial class Form1 : Form
+    {
+
+        private bool estado = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += Form1_Resize;
+            this.Luz.MouseClick += NotifyIcon1_MouseClick;
+            Luz.ContextMenuStrip = contextMenuStrip1;
+            this.porcentajeToolStripMenuItem.DragLeave += PorcentajeToolStripMenuItem_DragLeave;
+            ApagarBombilla();
+            this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void PorcentajeToolStripMenuItem_DragLeave(object sender, EventArgs e)
+        {
+            MessageBox.Show((sender as TrackBar).Value.ToString());
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                Luz.Visible = true;
+                Luz.ShowBalloonTip(100, "Luz"," ", ToolTipIcon.Info);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                Luz.Visible = false;
+            }
+        }
+
+        private void ApagarBombilla()
+        {
+            TcpClient socket = new TcpClient();
+            socket.Connect("192.168.1.5", 55443);
+            socket.Client.Send(Encoding.ASCII.GetBytes("{\"id\":1,\"method\":\"set_power\",\"params\":[\"off\", \"smooth\",500]}\r\n"));
+            socket.Close();
+        }
+        private void EncenderBombilla()
+        {
+            TcpClient socket = new TcpClient();
+            socket.Connect("192.168.1.5", 55443);
+            socket.Client.Send(Encoding.ASCII.GetBytes("{\"id\":1,\"method\":\"set_power\",\"params\":[\"on\", \"smooth\",500]}\r\n"));
+            socket.Close();
+        }
+        private void SetBrillo(Int32 brillo)
+        {
+            TcpClient socket = new TcpClient();
+            socket.Connect("192.168.1.5", 55443);
+            socket.Client.Send(Encoding.ASCII.GetBytes("{\"id\":1,\"method\":\"set_bright\",\"params\":["+brillo.ToString()+", \"smooth\",500]}\r\n"));
+            socket.Close();
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (estado)
+            {
+                Luz.Icon = new Icon("icon_off.ico");
+                Luz.Text = "Luz apagada";
+                ApagarBombilla();
+            }
+            else
+            {
+                Luz.Icon = new Icon("icon_on.ico");
+                Luz.Text = "Luz encendida";
+                EncenderBombilla();
+            }
+            estado = !estado;
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void NotifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                Luz.ContextMenuStrip.Show();
+            }
+        }
+
+        [ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.MenuStrip | ToolStripItemDesignerAvailability.ContextMenuStrip)]
+        public class TrackBarMenuItem : ToolStripControlHost
+        {
+            public TrackBar trackBar;
+
+            public TrackBarMenuItem() : base(new TrackBar())
+            {
+                this.trackBar = this.Control as TrackBar;
+            }
+        }
+
+        private void porcentajeToolStripMenuItem_Click(object sender, EventArgs e)
+        { 
+            SetBrillo((sender as TrackBarMenuItem).trackBar.Value*10);
+        }
+    }
+}
